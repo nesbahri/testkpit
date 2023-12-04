111fcc4b839bcb3f5ce845e63fe1105a1594f13e
nsrine <bahri.nesrine@gmail.com>
2023-12-04 15:07:50 +0100

    commit 1 in branch Dev1

---
+++ section15_Udemy.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+// Base class: Animal
+class Animal {
+private:
+    string name;
+public:
+    // base eclass constructor
+    Animal (const string &n):name(n){
+    cout<< "ANIMAL CONSTRUCTOR CALLED FOR " << name <<endl;
+
+    }
+
+    void eat() {
+        std::cout << "Animal is eating\n";
+    }
+};
+
+// Derived class: Dog
+class Dog : public Animal {
+private:
+    string race;
+public:
+     // derived class constructor using animal constructor argument: Animal(name)
+     Dog(const string &name, const string &breed): Animal(name),race(breed){
+        cout<< "DOG CONSTRUCTOR CALLED FOR "<< name <<" OF RACE "<< breed <<endl;
+
+     }
+    // Public member function of Dog
+    void Bark() {
+        
+        cout << "Woof! Woof!\n";
+    }
+};
+int main() {
+    Dog myDog("LISA","BERGER Allemand");
+    myDog.eat();
+    myDog.Bark();
+    
+    return 0;
+}
